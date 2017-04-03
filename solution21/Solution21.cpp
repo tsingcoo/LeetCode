1a36c6b5f8b1fe3be81cda5f55aa3da181bec6f0
wangqinglong <tsingcoo.wang@gmail.com>
2017-04-03 16:03:27 +0800

    改正了若干错误

--- solution21/Solution21.cpp
+++ solution21/Solution21.cpp
@@ -24,7 +24,7 @@ ListNode *Solution21::mergeTwoLists(ListNode *l1, ListNode *l2) {
 
     auto q = p;
 
-    while (l1->next != nullptr && l2->next != nullptr) {
+    while (l1 != nullptr && l2 != nullptr) {
         if (l1->val < l2->val) {
             p->next = l1;
             l1 = l1->next;
@@ -36,10 +36,11 @@ ListNode *Solution21::mergeTwoLists(ListNode *l1, ListNode *l2) {
         }
     }
 
-    if (l1->next != nullptr) {
-        p->next = l1->next;
-    } else {
-        p->next = l2->next;
+    if (l1 != nullptr) {
+        p->next = l1;
+    }
+    if (l2 != nullptr) {
+        p->next = l2;
     }
 
     return q;
