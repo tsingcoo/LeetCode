f847b0c82820686fc914e33c8b1dd14ac31ea026
wangqinglong <tsingcoo.wang@gmail.com>
2017-04-03 10:02:13 +0800

    递归没有理解写出来的辣鸡程序

--- solution199/Solution199.cpp
+++ solution199/Solution199.cpp
@@ -3,3 +3,32 @@
 //
 
 #include "Solution199.h"
+
+std::vector<int> Solution199::rightSideView(TreeNode *root) {
+
+    if (root == nullptr) {
+        return res;
+    }
+
+    helper(root, 0);
+    return res;
+
+}
+
+void Solution199::helper(TreeNode *root, int row) {
+    if (root == nullptr) {
+        return;
+    }
+    if (root->left != nullptr) {
+        if (res.size() < row + 1) {
+            res.push_back(root->val);
+        }
+        res[row] = root->val;
+    }
+    if (root->right != nullptr) {
+        if (res.size() < row + 1) {
+            res.push_back(root->val);
+        }
+        res[row] = root->val;
+    }
+}
--- solution199/Solution199.h
+++ solution199/Solution199.h
@@ -5,10 +5,22 @@
 #ifndef SOLUTION199_SOLUTION199_H
 #define SOLUTION199_SOLUTION199_H
 
+#include <vector>
 
+struct TreeNode{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x):val(x), left(nullptr), right(nullptr){}
+};
 
 class Solution199 {
+public:
+    std::vector<int> rightSideView(TreeNode* root);
+    void helper(TreeNode *root, int row);
 
+private:
+    std::vector<int> res;
 };
 
 
