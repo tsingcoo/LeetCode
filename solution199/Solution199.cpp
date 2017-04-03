5e57d83fbf4bbfd5f66c63279333c6db0572a144
wangqinglong <tsingcoo.wang@gmail.com>
2017-04-03 09:45:19 +0800

    init

---
+++ solution199/Solution199.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 王青龙 on 2017/4/3.
+//
+
+#include "Solution199.h"
---
+++ solution199/Solution199.h
@@ -0,0 +1,15 @@
+//
+// Created by 王青龙 on 2017/4/3.
+//
+
+#ifndef SOLUTION199_SOLUTION199_H
+#define SOLUTION199_SOLUTION199_H
+
+
+
+class Solution199 {
+
+};
+
+
+#endif //SOLUTION199_SOLUTION199_H
